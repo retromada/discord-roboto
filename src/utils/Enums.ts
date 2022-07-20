b18c33b58ff5b9a059dd648d88b7dc5ec6b5639a
ruj <214782@bk.ru>
2022-07-20 13:17:08 -0300

    chore: member actions

--- src/utils/Enums.ts
+++ src/utils/Enums.ts
@@ -1,4 +1,6 @@
 export enum ChannelAction {
+  MEMBER_JOIN = 'memberJoin',
+  MEMBER_LEAVE = 'memberLeave',
   DELETE_MESSAGES = 'deleteMessages',
   UPDATE_MESSAGES = 'updateMessages',
   BULK_DELETE_MESSAGES = 'bulkDeleteMessages'
