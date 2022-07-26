33f4a01adedcdd319a63ee59df3a12a2a06d8f89
ruj <214782@bk.ru>
2022-07-26 17:03:03 -0300

    chore: debug executed commands

--- src/listeners/interactionCreate.ts
+++ src/listeners/interactionCreate.ts
@@ -18,9 +18,14 @@ export default class InteractionCreate extends Listener {
     const commandName = interaction.commandName.toLowerCase()
 
     if (this.commands.has(commandName)) {
+      const { channel, guild, user } = interaction
       const command = this.commands.get(commandName)
       const context = new Context({ interaction })
 
+      this.logger.debug(
+        { labels: ['interactionCreate', command.name] },
+        `${command.category}/${command.name} run by ${user.tag} (${user.id}) on channel ${channel.name} (${channel.id}), guild ${guild.name} (${guild.id}).`
+      )
       command.executeCommand(context)
     }
   }
